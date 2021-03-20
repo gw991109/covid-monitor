0c158d4b9fde86694b01b7e70c731dd9eb28ab9b
linpao <edward9292@gmail.com>
2021-03-20 19:00:01 -0400

    add tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -5,3 +5,15 @@ def test_monitor():
 
 	assert response.status_code == 200
 	assert response.data == b'Welcome to the Covid Monitor!'
+
+def test_upload():
+	response = app.test_client.get('/upload')
+	assert response.status_code == 200
+
+def test_query():
+	response = app.test_client.get('/query')
+	assert response.status_code == 200
+
+def test_date():
+	response = app.test_client.get('/data')
+	assert response.status_code == 200
